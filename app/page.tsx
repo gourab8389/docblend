bb5afb6e634b082c5de72a3b96b31321002cd773
gourab8389 <deyg6988@gmail.com>
2024-07-18 22:11:08 +0530

    auth middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { SignIn } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-<div className="">
-  Home
-</div>
+    <div className="relative h-screen">
+      <div className="absolute top-0 left-0 w-full h-20 overflow-hidden bg-red-200 border shadow-lg">
+        <div className="absolute top-3 right-3"> 
+          <Link href={'/dashboard'}>
+            <Button className=" rounded-md font-bold text-white border border-b-red-950">Show Documents</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
