bbbf57b777f0ac09cd7f43ed289e262ba8108bbc
gourab8389 <deyg6988@gmail.com>
2024-07-18 23:26:49 +0530

    theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "./theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -28,7 +29,13 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange>
           {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
