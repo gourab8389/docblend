f3156fb9322bdf3f33699a8acaad09fdac1f0cd4
gourab8389 <deyg6988@gmail.com>
2024-07-18 21:42:00 +0530

    middleware changes

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+
+
+const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+
+    <div className="">
+        {children}
+    </div>
+}
+export default Layout
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
+  });
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)'
 
+  ]);
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
