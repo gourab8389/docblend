762fed2566aeea16da1c83cd427823917f02dab3
gourab8389 <deyg6988@gmail.com>
2024-07-18 23:02:34 +0530

    page changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,15 @@
 import { Button } from '@/components/ui/button';
-import {  SignOutButton } from '@clerk/nextjs';
+import {  SignOutButton, UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 function Dashboard() {
   return (
     <div className=''>
       <h1>Dashboard</h1>
-      <div className="absolute top-3 right-3">
-      <Button variant={'outline'}>
-        <SignOutButton/>
-      </Button>
+      <div className="absolute top-5 right-5">
+      
+        <UserButton/>
+      
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="relative h-screen">
-      <div className="absolute top-0 left-0 w-full h-20 overflow-hidden bg-red-200 border shadow-lg">
+      <div className="absolute top-0 left-0 w-full h-20 overflow-hidden border shadow-lg">
         <div className="absolute top-3 right-3"> 
           <Link href={'/dashboard'}>
-            <Button className=" rounded-md font-bold text-white border border-b-red-950">Show Documents</Button>
+            <Button variant={'secondary'} className="border border-black dark:border-white rounded-lg">Show Documents</Button>
           </Link>
         </div>
       </div>
